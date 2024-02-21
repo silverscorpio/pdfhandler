397d9f9e56a2023e0296dffda19969126517c77e
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-02-21 01:30:55 +0100

    match case logic, renaming, options removed

--- modules/handler.py
+++ modules/handler.py
@@ -18,28 +18,29 @@ HANDLE_PDF = {
 
 
 @click.command()
-@click.argument('pdf_op')
+@click.argument('pdf_operation')
 @click.argument('pdf_data')
-# @click.option('--level', help='compression level between 0 & 9 (max)', default=5, show_default=True)
-# @click.option('--quality', type=click.IntRange(0, 100, clamp=True),
-#               help='image quality between 0 & 100 (max)', default=50, show_default=True)
-def handler(pdf_op: str, pdf_data: str, *args, **kwargs):
-    match pdf_op := pdf_op.strip().lower():
+def handler(pdf_operation: str, pdf_data: str):
+    match pdf_operation := pdf_operation.strip().lower():
         case 'compress':
-            level = click.prompt("Required compression level (0-9 (max)): ",
-                                 type=click.IntRange(0, 9, clamp=True))
             parsed_pdf_data = parse_input_for_reduce(input_str=pdf_data)
-            HANDLE_PDF[pdf_op](parsed_pdf_data, level)
+            compression_level = click.prompt("Required compression level [0-9 (max)]: ",
+                                             type=click.IntRange(0, 9, clamp=True))
+            HANDLE_PDF[pdf_operation](parsed_pdf_data, compression_level)
 
         case 'image-compress':
-            quality = click.prompt("Required image quality level (0-100 (max)): ",
-                                   type=click.IntRange(0, 100, clamp=True))
             parsed_pdf_data = parse_input_for_reduce(input_str=pdf_data)
-            HANDLE_PDF[pdf_op](parsed_pdf_data, quality)
+            image_quality = click.prompt("Required image quality level [0-100 (max)]: ",
+                                         type=click.IntRange(0, 100, clamp=True))
+
+            HANDLE_PDF[pdf_operation](parsed_pdf_data, image_quality)
 
         case 'combine' | 'delete' | 'rearrange':
             parsed_pdf_data = parse_input_for_edit(input_str=pdf_data)
-            HANDLE_PDF[pdf_op](parsed_pdf_data)
+            HANDLE_PDF[pdf_operation](parsed_pdf_data)
+
+        case _:
+            raise click.BadParameter(f"Unknown PDF operation: '{pdf_operation}'")
 
 
 if __name__ == '__main__':
